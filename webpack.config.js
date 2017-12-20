6801ad8440794a6b0f487ce8ebf890f2d0376491
Sebastian Nyberg <sebastian@pandapropaganda.com>
2017-12-20 15:55:30 +0200

    1st commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+// webpack.config.js
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // Tell webpack to start bundling our app at src/index.tsx
+  entry: './src/index.tsx',
+  // Output our app to the dist/ directory
+  output: {
+    filename: 'app.js',
+    path: `${__dirname}/dist`,
+  },
+  // Emit source maps so we can debug our code in the browser
+  devtool: 'source-map',
+  // Tell webpack to run our source code through Babel
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ],
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    },
+    {
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    },
+    {
+      test: /\.svg$/,
+      exclude: /node_modules/,
+      loader: 'svg-react-loader',
+      query: {
+        classIdPrefix: '[name]-[hash:8]__',
+        propsMap: {
+          fillRule: 'fill-rule',
+          foo: 'bar',
+        },
+        xmlnsTest: /^xmlns.*$/,
+      }
+    }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  // Since Webpack only understands JavaScript, we need to
+  // add a plugin to tell it how to handle html files.
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.template.ejs',
+      inject: 'body',
+    }),
+    new CleanWebpackPlugin(['build']),
+    new CopyWebpackPlugin([
+      { from: 'src/images', to: 'images' },
+    ]),
+  ],
+};
